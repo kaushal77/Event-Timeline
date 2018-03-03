5315ccae2182f6f67e21dd5f8bd1a33ae2b09b80
kaushal <kaushal.chitlangia97@gmail.com>
2018-03-04 00:19:14 +0530

    first

---
+++ js/box.js
@@ -0,0 +1,390 @@
+//day1
+
+$(function () {
+        $("#dialog9").dialog({
+            modal: true,
+            autoOpen: false,
+            title: "***MYSTIQUE*** ",
+           
+            height: 350,
+            show: {
+        effect: "blind",
+        duration: 300
+      },
+      hide: {
+        effect: "explode",
+        duration: 500
+      }
+        });
+        $("#btnShow9").click(function () {
+            $('#dialog9').dialog('open');
+        });
+        
+    });
+
+
+ $(function () {
+        $("#dialog11").dialog({
+            modal: true,
+            autoOpen: false,
+            title: "***MYSTIQUE*** ",
+             
+            height: 350,
+            show: {
+        effect: "blind",
+        duration: 300
+      },
+      hide: {
+        effect: "explode",
+        duration: 500
+      }
+        });
+        $("#btnShow11").click(function () {
+            $('#dialog11').dialog('open');
+        });
+    });
+    
+  $(function () {
+        $("#dialog12").dialog({
+            modal: true,
+            autoOpen: false,
+            title: "***TREASURE HUNT*** ",
+             
+            height: 350,
+            show: {
+        effect: "blind",
+        duration: 300
+      },
+      hide: {
+        effect: "explode",
+        duration: 500
+      }
+        });
+        $("#btnShow12").click(function () {
+            $('#dialog12').dialog('open');
+        });
+    });
+    
+   $(function () {
+        $("#dialog13").dialog({
+            modal: true,
+            autoOpen: false,
+            title: "***WINNER*** ",
+             
+            height: 350,
+            show: {
+        effect: "blind",
+        duration: 300
+      },
+      hide: {
+        effect: "explode",
+        duration: 500
+      }
+        });
+        $("#btnShow13").click(function () {
+            $('#dialog13').dialog('open');
+        });
+    });
+    
+    $(function () {
+        $("#dialog14").dialog({
+            modal: true,
+            autoOpen: false,
+            title: "***WINNER*** ",
+             
+            height: 350,
+            show: {
+        effect: "blind",
+        duration: 300
+      },
+      hide: {
+        effect: "explode",
+        duration: 500
+      }
+        });
+        $("#btnShow14").click(function () {
+            $('#dialog14').dialog('open');
+        });
+    });
+    
+    $(function () {
+        $("#dialog15").dialog({
+            modal: true,
+            autoOpen: false,
+            title: "***WINNER*** ",
+             
+            height: 350,
+            show: {
+        effect: "blind",
+        duration: 300
+      },
+      hide: {
+        effect: "explode",
+        duration: 500
+      }
+        });
+        $("#btnShow15").click(function () {
+            $('#dialog15').dialog('open');
+        });
+    });
+    
+    $(function () {
+        $("#dialog16").dialog({
+            modal: true,
+            autoOpen: false,
+            title: "***WINNER***",
+             
+            height: 350,
+            show: {
+        effect: "blind",
+        duration: 300
+      },
+      hide: {
+        effect: "explode",
+        duration: 500
+      }
+        });
+        $("#btnShow16").click(function () {
+            $('#dialog16').dialog('open');
+        });
+    });
+    
+    $(function () {
+        $("#dialog17").dialog({
+            modal: true,
+            autoOpen: false,
+            title: "***WINNER*** ",
+             
+            height: 350,
+            show: {
+        effect: "blind",
+        duration: 300
+      },
+      hide: {
+        effect: "explode",
+        duration: 500
+      }
+        });
+        $("#btnShow17").click(function () {
+            $('#dialog17').dialog('open');
+        });
+    });
+    
+    $(function () {
+        $("#dialog18").dialog({
+            modal: true,
+            autoOpen: false,
+            title: "***WINNER*** ",
+             
+            height: 350,
+            show: {
+        effect: "blind",
+        duration: 300
+      },
+      hide: {
+        effect: "explode",
+        duration: 500
+      }
+        });
+        $("#btnShow18").click(function () {
+            $('#dialog18').dialog('open');
+        });
+    });
+    $(function () {
+        $("#dialog19").dialog({
+            modal: true,
+            autoOpen: false,
+            title: "***WINNER*** ",
+             
+            height: 350,
+            show: {
+        effect: "blind",
+        duration: 300
+      },
+      hide: {
+        effect: "explode",
+        duration: 500
+      }
+        });
+        $("#btnShow19").click(function () {
+            $('#dialog19').dialog('open');
+        });
+    });
+       
+ //day2
+ 
+ $(function () {
+        $("#dialog21").dialog({
+            modal: true,
+            autoOpen: false,
+            title: "***WINNER***  ",
+             
+            height: 350,
+            show: {
+        effect: "blind",
+        duration: 100
+      },
+      hide: {
+        effect: "explode",
+        duration: 100
+      }
+        });
+        $("#btnShow21").click(function () {
+            $('#dialog21').dialog('open');
+        });
+    });
+
+ $(function () {
+        $("#dialog22").dialog({
+            modal: true,
+            autoOpen: false,
+            title: "***WINNER***  ",
+             
+            height: 350,
+            show: {
+        effect: "blind",
+        duration: 100
+      },
+      hide: {
+        effect: "explode",
+        duration: 100
+      }
+        });
+        $("#btnShow22").click(function () {
+            $('#dialog22').dialog('open');
+        });
+    });
+ 
+ $(function () {
+        $("#dialog23").dialog({
+            modal: true,
+            autoOpen: false,
+            title: "***WINNER***  ",
+             
+            height: 350,
+            show: {
+        effect: "blind",
+        duration: 100
+      },
+      hide: {
+        effect: "explode",
+        duration: 100
+      }
+        });
+        $("#btnShow23").click(function () {
+            $('#dialog23').dialog('open');
+        });
+    });
+
+ 
+ //day3
+
+ $(function () {
+        $("#dialog31").dialog({
+            modal: true,
+            autoOpen: false,
+            title: "***WINNER***  ",
+             
+            height: 350,
+            show: {
+        effect: "blind",
+        duration: 100
+      },
+      hide: {
+        effect: "explode",
+        duration: 100
+      }
+        });
+        $("#btnShow31").click(function () {
+            $('#dialog31').dialog('open');
+        });
+    });             
+
+ $(function () {
+        $("#dialog32").dialog({
+            modal: true,
+            autoOpen: false,
+            title: "***WINNER***  ",
+             
+            height: 350,
+            show: {
+        effect: "blind",
+        duration: 100
+      },
+      hide: {
+        effect: "explode",
+        duration: 100
+      }
+        });
+        $("#btnShow32").click(function () {
+            $('#dialog32').dialog('open');
+        });
+    });
+ 
+ $(function () {
+        $("#dialog33").dialog({
+            modal: true,
+            autoOpen: false,
+            title: "***WINNER***  ",
+             
+            height: 350,
+            show: {
+        effect: "blind",
+        duration: 100
+      },
+      hide: {
+        effect: "explode",
+        duration: 100
+      }
+        });
+        $("#btnShow33").click(function () {
+            $('#dialog33').dialog('open');
+        });
+    });
+
+ //day4
+
+ $(function () {
+        $("#dialog41").dialog({
+            modal: true,
+            autoOpen: false,
+            title: "***WINNER***  ",
+             
+            height: 350,
+            show: {
+        effect: "blind",
+        duration: 300
+      },
+      hide: {
+        effect: "explode",
+        duration: 500
+      }
+        });
+        $("#btnShow41").click(function () {
+            $('#dialog41').dialog('open');
+        });
+    });
+
+
+//winner button
+
+$(function () {
+        $("#dialog").dialog({
+            modal: true,
+            autoOpen: false,
+            title: "***WINNER***  ",
+             
+            height: 350,
+            show: {
+        effect: "blind",
+        duration: 300
+      },
+      hide: {
+        effect: "explode",
+        duration: 500
+      }
+        });
+        $("#btnShow").click(function () {
+            $('#dialog').dialog('open');
+        });
+    });
